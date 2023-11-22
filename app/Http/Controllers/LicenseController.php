102388046194844c48660770165353713b21cd99
nguyenvantoi <toi.nguyen@sotatek.com>
2023-11-22 12:53:55 +0700

    firest

---
+++ app/Http/Controllers/LicenseController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FleetCart\Http\Controllers;
+
+use FleetCart\License;
+use Illuminate\Routing\Controller;
+use FleetCart\Http\Middleware\RedirectIfShouldNotCreateLicense;
+
+class LicenseController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(RedirectIfShouldNotCreateLicense::class);
+    }
+
+    public function create()
+    {
+      echo 333;
+        return view('license.create');
+    }
+
+    public function store(License $license)
+    {
+        request()->validate([
+            'purchase_code' => 'required',
+        ], [
+            'required' => 'The purchase code field is required.',
+        ]);
+
+        $license->activate(request('purchase_code'));
+
+        return redirect()->intended();
+    }
+}
